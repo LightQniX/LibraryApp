52508d9b6c0aaf4223253eed094ee122c84a80a3
LightQniX <lightqnix@gmail.com>
2022-03-25 17:41:58 +0300

    first commit1

---
+++ main.py
@@ -0,0 +1,78 @@
+from flask import Flask, render_template, request, flash, render_template_string, redirect, Response
+import psycopg2
+import json
+
+
+
+dbhost = 'ec2-54-216-17-9.eu-west-1.compute.amazonaws.com'
+dbname = 'djrs2mt5np83b'
+dbuser = 'womlylvmxosadm'
+dbport = '5432'
+dbpass = '300cba31baa710789df66e6dbda88f1183d92462e00f8c92ff44687a27ac601c'
+con = psycopg2.connect(dbname=dbname, port=dbport, user=dbuser, password=dbpass, host=dbhost)
+con.set_session(autocommit=True)
+cur = con.cursor()
+
+
+
+
+
+
+
+
+
+app = Flask(__name__)
+
+
+
+@app.route("/getBook/<id>", methods=["POST", "GET"])
+def getBook(id):
+
+    cur.execute(f'select * from books where id={id}')
+    book = cur.fetchall()
+
+    if book:
+        book = book[0]
+        data = {
+            'id': book[0],
+            'title': book[1],
+            'author': book[2],
+            'year': book[3],
+            'description': book[4]
+        }
+
+        return json.dumps(data), 200
+
+    else:
+        return {'library': 'book not found'}, 404
+
+
+
+
+@app.route("/getBookList", methods=["POST", "GET"])
+def getBookList():
+
+    cur.execute(f'select * from books')
+    books = cur.fetchall()
+    print(books)
+    data = []
+
+    if books:
+        for book in books:
+            data.append(
+                {
+                    'id': book[0],
+                    'title': book[1],
+                    'author': book[2],
+                    'year': book[3],
+                    'description': book[4]
+                }
+            )
+
+            return json.dumps(data), 200
+
+    else:
+        return {'library': 'library is empty'}, 404
+
+
+app.run(debug=True, host='localhost', port=5001)
